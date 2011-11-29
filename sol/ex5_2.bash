001e1f192109a1cca26b0187f09d5039ffc1130b
ynonp <ynonperek@gmail.com>
2011-11-29 10:55:16 +0200

    BUgfix and name fixes

--- sol/ex5_2.bash
+++ sol/ex5_2.bash
@@ -4,11 +4,11 @@
 # running on the server
 #
 
-read -p "Process name: " pname
-count=$(ps -e| tr -s ' ' | cut -d ' ' -f4 | grep $pname | wc -l | tr -d ' ')
+read -p "Process name: " PNAME
+COUNT=$(ps -e| tr -s ' ' | cut -d ' ' -f5 | grep $PNAME | wc -l | tr -d ' ')
 
-if [[ $count == "0" ]]; then
+if [[ $COUNT == "0" ]]; then
     echo "No procs found"
 else
-    echo "Found $count processes"
+    echo "Found $COUNT processes"
 fi
